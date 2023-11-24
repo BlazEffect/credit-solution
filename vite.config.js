4b19c320fee96d9e3b8e62136318f3465b79bbae
BlazEffect <kamalinigor@gmail.com>
2023-11-25 01:04:59 +0300

    project setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import 'dotenv/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: process.env.SERVER_IP,
+    port: process.env.SERVER_PORT,
+    watch: {
+      usePolling: true
+    },
+    proxy: {
+      '/api': {
+        target: process.env.API_SERVER_URL,
+        changeOrigin: true,
+        secure: false,
+      }
+    },
+    cors: false
+  }
 })
